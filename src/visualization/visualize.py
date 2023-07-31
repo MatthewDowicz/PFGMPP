276ebce4a38d3b2bf600e9b03bc36f39ddd191a3
MatthewDowicz <mjdowicz@gmail.com>
2023-07-31 16:23:00 -0700

    Updated the visualization function, so that it works with all the other updates that were implemented.

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -8,28 +8,25 @@ rcParams['mathtext.fontset'] = 'dejavuserif'
 rcParams['font.family'] = 'serif'
 
 
-def sampling2D(prior_data, true_data, generated_data, std=0.2, mean_scale=1):
+def sampling2D(prior_data, true_data, sampled_data, std=0.2, mean_scale=1):
     var_data = std**2 + mean_scale**2
     std_data = np.sqrt(var_data)
 
-    generated_data = prior_data * std_data
+    generated_data = sampled_data * std_data
 
     fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(16, 4))
     
     ax0.scatter(prior_data[:, 0], prior_data[:, 1], label='Generated Data', alpha=0.5)
-    # ax0.set_title(f'$D={D}$', fontsize=20)
     ax0.set_xlabel('$x_0$', fontsize=15)
     ax0.set_ylabel('$x_1$', fontsize=15)
     ax0.legend()
     
     ax1.scatter(generated_data[:, 0], generated_data[:, 1], label='Denoised Generated data', alpha=0.5)
-    # ax1.set_title(f'$D={D}$', fontsize=20)
     ax1.set_xlabel('$x_0$', fontsize=15)
     ax1.set_ylabel('$x_1$', fontsize=15)
     ax1.legend()
     
     ax2.scatter(true_data[:, 0], true_data[:, 1], label='True Data', alpha=0.5, color='orange')
-    # ax2.set_title(f'$D={D}$', fontsize=20)
     ax2.set_xlabel('$x_0$', fontsize=15)
     ax2.set_ylabel('$x_1$', fontsize=15)
     ax2.legend()
