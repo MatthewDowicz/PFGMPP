f447f382f7225641d550beba028331fbb0c2475b
MatthewDowicz <mjdowicz@gmail.com>
2023-07-30 19:35:53 -0700

    Added a setup.py file to allow the src subdirectory to be a pip installable package.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='PFGMPP',
     version='0.1',
-    packages=find_packages(where='src'),
+    packages = find_packages(where='/home/mdowicz/PFGMPP/src'),
     package_dir={'': 'src'},
 )
